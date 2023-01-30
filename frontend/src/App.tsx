98691602477736605ea29b7eae2f58ec7ae853a3
Jose Carlos <josecarloscjj@gmail.com>
2023-01-30 18:10:07 -0300

    feat: Header card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
-   <h1>DSFilter</h1>
+   <Header />
   )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+export default function Header() {
+  return (
+    <>
+      <div className="container title-header">
+        <h1>DSFilter</h1>
+        <p>6 produtos</p>
+      </div>
+    </>
+  );
+}
